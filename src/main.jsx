7a8518619e701fbb0b3fae7bb0dd1a8e49584b02
Alejandro2097 <alejo12381@hotmail.es>
2023-06-14 21:47:54 -0500

    bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './index.css'
+import { HeroesApp } from './HeroesApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HeroesApp />
   </React.StrictMode>,
 )
