9b68fdf9841009fd6a4d313fbe5c687d912596b2
Alejandro2097 <alejo12381@hotmail.es>
2023-06-15 20:49:17 -0500

    heroes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { MarvelPage } from '../heroes/pages/MarvelPage'
 import { DcPage } from '../heroes/pages/DcPage'
 import { LoginPage } from '../auth/pages/LoginPage'
@@ -11,6 +11,7 @@ export const AppRouter = () => {
             <Route path="dc" element={<DcPage/>}/>
 
             <Route path="login" element={<LoginPage/>}/>
+            <Route path="/" element={<Navigate to="marvel"/>}/>
         </Routes>
     </>
   )
