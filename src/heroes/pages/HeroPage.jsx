9972b2912ec3216b588e499bb125c425b873a894
Alejandro2097 <alejo12381@hotmail.es>
2023-06-19 18:32:49 -0500

    Pagina de los heroes

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,33 @@
-import { useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
   const { id } = useParams();
 
+  const hero = getHeroById(id);
+
+  if(!hero){
+    return <Navigate to="/marvel"/>;
+  }
   return (
-    <div>HeroPage</div>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={`/assets/heroes/${ id }.jpg`}
+          alt={hero.superhero} 
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group">
+          <li className="list-group-item"><b>Alter ego: {hero.alter_ego}</b></li>
+          <li className="list-group-item"><b>Publisher: {hero.publisher}</b></li>
+          <li className="list-group-item"><b>First appearance: {hero.first_appearance}</b></li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+      </div>
+    </div>
   )
 }
