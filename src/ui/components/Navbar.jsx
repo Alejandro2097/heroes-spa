416671d2062e681bf39377ea12449f0689a07dd1
Alejandro2097 <alejo12381@hotmail.es>
2023-06-19 19:15:49 -0500

    useForm

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -35,6 +35,12 @@ export const Navbar = (props) => {
                     >
                         DC
                     </NavLink>
+                    <NavLink 
+                         className={ ({ isActive }) => `nav-item nav-link ${ isActive ? 'active' : '' }`}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
