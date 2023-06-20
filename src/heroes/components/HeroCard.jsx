c431e56ed9440543ea68c6a6758a45de8f8233ca
Alejandro2097 <alejo12381@hotmail.es>
2023-06-19 19:04:36 -0500

    animaciones

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -21,7 +21,7 @@ export const HeroCard = (
     const heroeUrl = `/assets/heroes/${ id }.jpg`;
     // const charactersByHero =( <p>{characters}</p>);
   return (
-    <div className="col">
+    <div className="col animate__animated animate__fadeIn">
         <div className="card">
             <div className="row no-gutters">
                 <div className="col-4">
@@ -38,7 +38,7 @@ export const HeroCard = (
                         <p className="card-text">
                             <small className="text-muted">{first_appearance}</small>
                         </p>
-                        <Link to={`/hero${id}`}>
+                        <Link to={`/hero/${id}`}>
                             mas...
                         </Link>
                     </div>
