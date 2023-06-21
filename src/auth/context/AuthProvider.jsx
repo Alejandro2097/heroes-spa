c248bb78407b46bf00886ecfecf0f154e32bd176
Alejandro2097 <alejo12381@hotmail.es>
2023-06-20 22:32:29 -0500

    auth

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,16 +1,31 @@
 import { useReducer } from "react";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
+import { types } from "../types/types";
 
 const initialState = {
     logged: false,
 }
 // eslint-disable-next-line react/prop-types
 export const AuthProvider = ({children}) => {
-    const [state, dispatch] =  useReducer(authReducer, {});
+    const [authState, dispatch] =  useReducer(authReducer, {});
+
+    const login = (name = '') => {
+      const action = {
+        type: types.login,
+        payload: {
+          id: 'ABC',
+          name: name
+        }
+      }
+      dispatch(action);
+    }
   return (
-    <AuthContext value={{ }}>
+    <AuthContext.Provider value={{
+      ...authState,
+      login: login
+     }}>
         {children}
-    </AuthContext>
+    </AuthContext.Provider>
   );
 }
