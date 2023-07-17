61ffe7ef7f313cfeb2293928c7f08ffff5a32f00
Alejandro2097 <alejo12381@hotmail.es>
2023-07-16 19:18:17 -0500

    im back

--- test/router/PublicRoute.test.jsx
+++ test/router/PublicRoute.test.jsx
@@ -1,19 +1,28 @@
 import { render, screen } from "@testing-library/react";
-import { PublicRoute } from "../../src/router/PublicRoute";
+import { MemoryRouter } from "react-router-dom";
+
 import { AuthContext } from "../../src/auth";
+import { PublicRoute } from "../../src/router/PublicRoute";
 
 describe('Pruebas en <PublicRoute/>', () => {
     test('debe de mostrar el children si no esta autenticado', () =>{
         const contextValue = {
-            logged: false
+            logged: false,
+            user: {
+                name: 'Strider',
+                id: 'ABC123'
+            }
         }
         render(
             <AuthContext.Provider value={contextValue}>
-                <PublicRoute>
-                    <h1>Ruta publica</h1>
-                </PublicRoute>
+                <MemoryRouter initialEntries={['/login']}>
+                    <PublicRoute>
+                        <h1>Ruta publica</h1>
+                    </PublicRoute>
+                </MemoryRouter>
             </AuthContext.Provider>
         );
+        screen.debug();
         expect(screen.getByText('Ruta publica')).toBeTruthy();
     });
     
